6fd3537896c7795369094f13f1df0b8b6a34cbae
Unnikrishnan R <unnikrishnanr@Mac.lan>
2026-06-07 01:15:28 +0100

    Fix BM convention centre name

--- petals.js
+++ petals.js
@@ -47,7 +47,7 @@ const events = {
   },
   sangeeth: {
     kicker: "Sangeeth",
-    title: "Sangeeth Celebration",
+    title: "BM Convention Centre",
     location: "Ambalathara, Trivandrum",
     detail: "An evening of music, dance, and celebration",
     mapUrl: "https://maps.app.goo.gl/STb2p3m6F3wfmBWU6?g_st=iw"
